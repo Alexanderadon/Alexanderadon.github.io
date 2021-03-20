998794c8b02b379504512ebe85763299c8a562a7
Alexander <63007496+Alexanderadon@users.noreply.github.com>
2021-03-20 16:49:05 +0600

    Delete assets directory

--- assets/js/collaps.js
+++
@@ -1,8 +0,0 @@
-$("[data-collapse]").on("click", function(event) {
-    event.preventDefault();
-
-    var $this = $(this),
-    elementId = $this.data('collapse');
-
-    $this.toggleClass("active");
-});
