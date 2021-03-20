a0a45dc4e2500b93f21ada73b9dcdc4360014964
Alexander <63007496+Alexanderadon@users.noreply.github.com>
2021-03-21 01:42:13 +0600

    Delete registration directory

--- registration/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".dws-form").on("click", ".tab", function () {
-    //    Удаляем класс
-        $(".dws-form").find(".active").removeClass("active");
-    //    Добавляем класс
-        $(this).addClass("active");
-        $(".tab-form").eq($(this).index()).addClass("active");
-    });
-});
