2e511b5a2d55fd17627557f94ee2416cd279421f
Alexander <63007496+Alexanderadon@users.noreply.github.com>
2021-09-30 13:48:00 +0600

    add slider up&down
    add slider

---
+++ Projects__up&down--slider/script.js
@@ -0,0 +1,61 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+
+const container = document.querySelector('.container');
+
+const sidebar = document.querySelector('.sidebar');
+const mainslide = document.querySelector('.main-slide');
+
+const slidecount = mainslide.querySelectorAll('div').length
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidecount - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+})
+
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+})
+
+function changeSlidemouse(direction) {
+    setInterval(() => {
+    if (direction === 'mouseSlideUp') {
+        activeSlideIndex++
+        if(activeSlideIndex === slidecount)
+            {
+            activeSlideIndex = 0;
+        }
+    }
+    const height = container.clientHeight;
+
+    mainslide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}, 3500);
+}
+changeSlidemouse('mouseSlideUp');
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex === slidecount)
+            {
+            activeSlideIndex = 0;
+        }
+    }   else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidecount - 1
+        }
+    }
+
+    const height = container.clientHeight;
+
+    mainslide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
+
