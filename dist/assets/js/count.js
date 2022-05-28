ffa7cc02d417c5cc2cdfa2cd8cdf57a828393248
Alexander <63007496+Alexanderadon@users.noreply.github.com>
2022-05-28 23:25:43 +0600

    Add files via upload

---
+++ dist/assets/js/count.js
@@ -0,0 +1,16 @@
+let img = document.querySelectorAll('.photo');
+let countPhoto = 0;
+let imgcount = document.querySelector('[datephoto]');
+
+imgcount.innerHTML =  img.length;
+console.log(countPhoto);
+
+
+img.forEach(element => {
+  let arr = [];
+  // console.log(element.length);
+
+  // count += element.value;
+
+});
+
